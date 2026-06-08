d38c56f55282ec1a0ba84aaef276c01479f926c4
mariavg2409 <maalonsovega@gmail.com>
2026-06-08 18:26:51 +0200

    Fix script to launch training

--- scripts/launch_train.sh
+++ scripts/launch_train.sh
@@ -13,6 +13,7 @@ for emb_path in "${!RUN_NAMES[@]}"; do
     run_name="${RUN_NAMES[$emb_path]}"
 
     python train.py config/train_delphi_demo.py \
+        --device=cuda \
         --wandb_log=True \
         --wandb_run_name="$run_name" \
         --out_dir="out/$run_name" \
@@ -24,6 +25,7 @@ done
 
 
 python train.py config/train_delphi_demo.py \
+    --device=cuda \
     --wandb_log=True \
     --wandb_run_name=delphi \
     --out_dir="out/delphi" \
